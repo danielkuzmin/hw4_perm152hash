76137f736174474a084c0402ce26d5eb4aa1325d
Daniel Kuzmin <danielkuzmin@gmail.com>
2019-10-20 17:58:54 -0700

    Initial commit

--- hw4_perm152hash.c
+++ hw4_perm152hash.c
@@ -4,16 +4,19 @@
  October 20 2019
  Brief Description:
  This file contains the code for part A of the fourth assignment.
- This encrypts an array of 64 bits with the given algorithm.
- from the assignment, using Intel's SSE registers.
+ This is a sponge-based cryptographic hash that uses Perm152, constructed using the assignment PDF'S pseudocode
  Tested on Athena, compiled like so:
  clang -g -O0 -std=c99 -Werror -Wall -Wextra -Wconversion -fsanitize=address hw4_perm152hash.c
 */
 
+// REMOVE THIS INCLUDE BEFORE TURING IN
 #include "perm152.c"
+// REMOVE THE ABOVE INCLUDE BEFORE TURNING IN
+#include <stdio.h>
+
+// Perm152 is prototyped below but defined in a separate file
+void perm152(unsigned char *in, unsigned char *out);
 
 void perm152hash(unsigned char *m, int mbytes, unsigned char *res) {
 
 }
-
-void perm152(unsigned char *in, unsigned char *out);
--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include "hw4_perm152hash.c"
 
+// The main is just to test perm152hash
 int main() {
-    printf("Hello, World!\n");
+    printf("Printing out buffer:\n");
+    unsigned char buf[72];
+
+    for (int i = 0; i < 72; i++) {
+        buf[i] = (unsigned char)i;
+    }
+
+    unsigned char out[32];
+    perm152hash(buf, 72, out);
+
+    for (int i = 0; i < 32; i++) {
+        printf("%x\n",out[i]);
+    }
+
     return 0;
 }
