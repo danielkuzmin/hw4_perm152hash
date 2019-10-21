cc8d7940075ba1a2349e3b0ec770739eaf1fe8c4
danielkuzmin <danielkuzmin@gmail.com>
2019-10-20 22:12:34 -0700

    Fixed a comment

--- hw4_perm152hash.c
+++ hw4_perm152hash.c
@@ -3,7 +3,7 @@
  Class code: 1618
  October 20 2019
  Brief Description:
- This file contains the code for part A of the fourth assignment.
+ This file contains the code for the fourth assignment.
  This is a sponge-based cryptographic hash that uses Perm152, constructed using the assignment PDF'S pseudocode
  Tested on Athena, compiled like so:
  clang -g -O0 -std=c99 -Werror -Wall -Wextra -Wconversion -fsanitize=address hw4_perm152hash.c
