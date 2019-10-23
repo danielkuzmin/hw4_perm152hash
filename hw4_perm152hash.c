02811d8a0784cf3bbf07ffe6aef7be5e3267fcb6
Daniel Kuzmin <danielkuzmin@gmail.com>
2019-10-22 21:14:01 -0700

    penultimate

--- hw4_perm152hash.c
+++ hw4_perm152hash.c
@@ -15,7 +15,31 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#define Capacity 32
+#define Rate 32
+#define Output 32
+#define Param64 64
 
+void arrClear(unsigned char* arr, unsigned long len)
+{
+    memset(arr, 0, len);
+}
+void arrCopy(unsigned char* arrsrc, unsigned char* arrtgt, unsigned long len)
+{
+ memcpy(arrtgt, arrsrc, len);
+}
+void arrPrint(unsigned char* msg, unsigned long len)
+{
+    for(unsigned long i = 0; i < len; i++)
+    {
+        printf("%c", msg[i]);
+    }
+    printf("\n");
+}
+void printMsg(unsigned char* msg)
+{
+    printf("%s\n", msg);
+}
 // Perm152 is prototyped below but defined in a separate file
 void perm152(unsigned char *in, unsigned char *out);
 
@@ -27,146 +51,41 @@ void xor(unsigned char *dst, unsigned char *src, int num_bytes) {
 }
 
 void perm152hash(unsigned char *m, int mbytes, unsigned char *res) {
-//if(*m == 'c') return;
-//if(mbytes == 0) return;
-//if(res[0] == '0') return;
-//
-//    for (int i = 0; i < mbytes; i++)
-//    {
-//        res[i] = m[i];
-//    }
-//
-//unsigned char ctemp[32] = "ABCDEFGHIDKLMLNOPQRSTUVWXYZabcde";
-
-
-    //Prepare arrays
-//    int nCapacity = 32;
-//    int nRate = 32;
-//    int nSize = 32;
-//    unsigned char cState[32];
-//    unsigned char cPadded[10 * nRate];
-/*
-    xor(ctemp, m, mbytes);
-
-    for(int i = 0; i < mbytes; i++)
-    {
-        printf("%c", ctemp[i]);
-
-    }
-    printf("\n");
-
-    unsigned char ctemp2[64];
-    unsigned char ctemp3[64];
-    memset(ctemp3, 0, 64);
-    memset(ctemp2, 0, 64);
-
-    for(int i = 0; i < 32; i++)
-    {
-        ctemp2[i] = ctemp[i];
-    }
-
-    perm152(ctemp2, ctemp3);
-
-    for(int i = 0; i < mbytes; i++)
-    {
-        printf("%c", ctemp3[i]);
-    }
-    printf("\n");*/
-
-
-
     //absorb
-    unsigned long MPaddedLen = (unsigned long) (32 + (10 * mbytes));
+    unsigned long MPaddedLen = (unsigned long) (Capacity + (10 * mbytes));
     unsigned char MPadded[MPaddedLen];
-
     memset(MPadded, 0, MPaddedLen);
-
-    for(int i = 0; i < mbytes; i++)
-    {
-        MPadded[i] = m[i];
-    }
-
-    int n = mbytes / 32; //need a variable for rate
-
-
-    unsigned char chunk[32];
-    unsigned char result[32];
-    unsigned char ctemp2b[64], ctemp3b[64];
-    memset(result, 0, 32);
+    arrCopy(m, MPadded, (unsigned long) mbytes);
+    int n = mbytes/Rate;
+    unsigned char chunk[Capacity];
+    unsigned char result[Output];
+    unsigned char ctemp2b[Param64], ctemp3b[Param64];
+    memset(result, 0, Output);
 
     // This loop need to step through MPadded extracting chunks
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < 32; j++)
+        for(int j = 0; j < Capacity; j++)
         {
             chunk[j] = MPadded[j];
-
         }
         xor(result, chunk, 32);
-
-
-        memset(ctemp2b, 0, 64);
-        memset(ctemp3b, 0, 64);
-
-        for(int z = 0; z < 32; z++)
-        {
-            ctemp2b[z] = chunk[z];
-        }
-
+        memset(ctemp2b, 0, Param64);
+        memset(ctemp3b, 0, Param64);
+        arrCopy(ctemp2b, chunk, Rate);
         perm152(ctemp2b, ctemp3b);
-
-        for(int x = 0; x < 32; x++ )
-        {
-            result[x] = ctemp3b[x];
-            res[x] = ctemp3b[x];
-
-            //printf("%d ", result[x]);
-        }
-        printf("\n");
-
-
-
+        arrCopy(result, ctemp3b, Rate);
     }
 //squeeze
-
-    //Capacity
-    int c = 32;
-    //Rate
-//    int r = 32;
-    //Output length
-    int b = 32;
-
     while(true)
     {
-        for(int i = 0; i < c; i++){
+        for(int i = 0; i < Capacity; i++){
             res[i] = result[i];
-            if(i >= b-1)
+            if(i >= Output - 1)
             {
                 return;
             }
         }
         perm152(ctemp2b, ctemp3b);
     }
-
-
-
-    //*m, mbytes, *res parameter names
-
-
-    // Initialize an array of 32 bytes that are all zero
-    //unsigned char zeroArray[32];
-    //memset(zeroArray, 0 ,32);
-
-/*    while (mbytes >= 32) {
-        unsigned char toProcess[64];
-        // Copy the first 32 bytes of m to the processing array
-        for (int i = 0; i < 32; i++) {
-            toProcess[i] = m[i];
-            res[i] = m[i];
-        }
-        mbytes = mbytes - 32;
-        m += 32;
-    }
-*/
-
 }
--- main.c
+++ main.c
@@ -3,18 +3,10 @@
 
 // The main is just to test perm152hash
 int main() {
-    unsigned char buf[32] = "THIS IS A TEST OF HASH 32 bytes.";
-    int r;
-    int c;
-    int b;
-    r = c = b = 32;
-
-    for (int i = 0; i < c; i++) {
-        //buf[i] = (unsigned char)i;
-    }
-
+    unsigned char buf[72] = "THIS IS A TEST OF HASH 72 bytes. The last output chunk should be padded.";
     unsigned char out[32];
-    perm152hash(buf, 32, out);
+
+    perm152hash(buf, 72, out);
 
     for (int i = 0; i < 32; i++) {
         printf("%c",buf[i]);
